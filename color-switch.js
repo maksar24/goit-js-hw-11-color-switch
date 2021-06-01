a801aa94773345d215df51a9b2b1c1baac270f33
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-06-01 10:03:22 +0200

    startihg hw 11

--- color-switch.js
+++ color-switch.js
@@ -1,4 +1,4 @@
-const colors = [
+export const colors = [
   '#FFFFFF',
   '#2196F3',
   '#4CAF50',
@@ -6,3 +6,17 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const start = document.querySelector('button[data-action="start"]');
+const stop = document.querySelector('button[data-action="stop"]');
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// start.addEventListener("click", switcher);
+
+console.log(colors)
+
+
+
